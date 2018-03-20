199348f25fa71f0dba9a3860d47bb21965f1d5c5
Vaaibhavi Singh <vaaibhavisingh@gmail.com>
2018-03-21 01:57:22 +0530

    Add Changes

--- Node_HTTP/index.js
+++ Node_HTTP/index.js
@@ -1,14 +1,58 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-	console.log(req.headers);
+	//console.log(req.headers);
 
-	res.statusCode = 200;
-	res.setHeader('Content-Type','text/html');
-	res.end('<html><body><h1>Hello Mahi!</h1</body></html>');
+	//res.statusCode = 200;
+	//res.setHeader('Content-Type','text/html');
+	//res.end('<html><body><h1>Hello World!</h1></body></html>');
+
+	console.log("Request for "+ req.url +" by method "+ req.method);
+
+	if(req.method == 'GET'){
+		var fileURL;
+		if(req.url == '/'){
+			fileURL = '/index.html';
+		}
+		else{
+			fileURL = req.url;
+		}
+
+		var filePath = path.resolve('./public'+fileURL);
+		const fileExt = path.extname(filePath);
+
+		if(fileExt == '.html'){
+			fs.exists(filePath, (exists) => {
+				if(!exists){
+					res.statusCode = 404;
+					res.setHeader('Content-Type', 'text/html');
+					res.end('<html><body><h1>Error 404 : '+ fileURL +' not found!</h1></body></html>');
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/html');
+				fs.createReadStream(filePath).pipe(res);
+			});
+        }
+        else{
+        	res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/html');
+			res.end('<html><body><h1>Error 404 : '+ fileURL +' not an HTML file!</h1></body></html>');
+			return;
+        }
+	}
+
+	else{
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<html><body><h1>Error 404 : '+ req.method +' not supportred!</h1></body></html>');
+		return;
+	}
 })
 
 server.listen(port, hostname, () => {
